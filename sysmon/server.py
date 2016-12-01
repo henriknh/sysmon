e925d119685556a26294ac14ed10b3412107d4fb
Henrik <henrik.nilsson.harnert@gmail.com>
2016-12-01 08:54:39 +0100

    fixed disk usage on linux

--- sysmon/server.py
+++ sysmon/server.py
@@ -1,8 +1,38 @@
 import psutil
 import platform
+import os
 from time import time
 from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
+
+if hasattr(os, 'statvfs'):  # POSIX
+	def disk_usage(path):
+		st = os.statvfs(path)
+		free = st.f_bavail * st.f_frsize
+		total = st.f_blocks * st.f_frsize
+		used = (st.f_blocks - st.f_bfree) * st.f_frsize
+		return used/total*100 #[total, used, free]
+
+elif os.name == 'nt':       # Windows
+    import ctypes
+    import sys
+
+    def disk_usage(path):
+        _, total, free = ctypes.c_ulonglong(), ctypes.c_ulonglong(), \
+                           ctypes.c_ulonglong()
+        if sys.version_info >= (3,) or isinstance(path, unicode):
+            fun = ctypes.windll.kernel32.GetDiskFreeSpaceExW
+        else:
+            fun = ctypes.windll.kernel32.GetDiskFreeSpaceExA
+        ret = fun(path, ctypes.byref(_), ctypes.byref(total), ctypes.byref(free))
+        if ret == 0:
+            raise ctypes.WinError()
+        used = total.value - free.value
+        return used/total*100 #[total.value, used, free.value]
+else:
+    raise NotImplementedError("platform not supported")
+
+disk_usage.__doc__ = __doc__
 
 @app.route('/')
 def root():
@@ -10,7 +40,6 @@ def root():
 
 @app.route('/api', methods=['POST'])
 def api():
-
 	json = request.json
 
 	if ('method' and 'data') not in json:
@@ -31,7 +60,7 @@ def api():
 				hdd = {}
 				for disk in psutil.disk_partitions():
 					try:
-						hdd[disk.device] = psutil.disk_usage(disk.device).percent
+						hdd[disk.mountpoint] = "{0:.1f}".format(disk_usage(disk.mountpoint))
 					except OSError:
 						pass
 					i += 1
@@ -39,7 +68,7 @@ def api():
 			if hw == 'uptime':
 				resp_data['uptime'] = time() - psutil.boot_time()
 
-		print resp_data
+		print (resp_data)
 
 	return jsonify(results=resp_data)
 
